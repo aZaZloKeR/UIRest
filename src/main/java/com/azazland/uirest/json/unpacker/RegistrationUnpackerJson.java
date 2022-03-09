d2db37f8ccdf9a11c0651f4617fbfb45dbe1c8ca
Nikita <azazloker.dadaya@gmail.com>
2022-03-09 18:20:35 +0300

    change RestController

--- src/main/java/com/azazland/uirest/json/unpacker/RegistrationUnpackerJson.java
+++ src/main/java/com/azazland/uirest/json/unpacker/RegistrationUnpackerJson.java
@@ -2,10 +2,25 @@ package com.azazland.uirest.json.unpacker;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import net.minidev.json.JSONObject;
+
+import java.util.Date;
 
 @Getter
 @Setter
+@ToString
 public class RegistrationUnpackerJson {
+    private String first_name;
+    private String last_name;
+    private String third_name;
+    private Date birthday;
+    private String phone_number;
+    private JSONObject address;
+    private String street_type;
+    private String house;
+    private String corps;
+    private String apartment;
     private String login;
     private String password;
 
