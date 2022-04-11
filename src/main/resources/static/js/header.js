40ad034cb275bd77f5a0293624e61576ba9e6be5
Alex <62909177+Hesher73@users.noreply.github.com>
2022-04-11 13:42:18 +0300

    change file path (+ Vlad thymeleaf)

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -3,9 +3,9 @@ var headerText =
     '    <ul class="nav">\n' +
     '        <li class="liHeader">\n' +
     '            <div id="logo_info">\n' +
-    '                <img id="logo_img" src="./img/Шапка/Vect.png" />\n' +
-    '                <img id="logo_line" src="./img/Шапка/Line%201.png" />\n' +
-    '                <a href="index.htm"><p id="sait_name"><strong>Инициативы<br/>жителей Дубны</strong></p></a>\n' +
+    '                <img id="logo_img" src="../static/img/Vect.png" th:href="@{/img/Vect.png}" />\n' +
+    '                <img id="logo_line" src="../static/img/Line%201.png" th:href="@{/img/Line%201.png}" />\n' +
+    '                <a href="./index.html"><p id="sait_name"><strong>Инициативы<br/>жителей Дубны</strong></p></a>\n' +
     '            </div>\n' +
     '        </li>\n' +
     '        <li class="liHeader">\n' +
@@ -16,7 +16,7 @@ var headerText =
     '        <li class="liHeader"><a class="nav" href="#">Предложить инициативу</a></li>\n' +
     '        <li class="liHeader"><a class="nav" href="#">Правила работы</a></li>\n' +
     '        <li class="liHeader"><a class="nav" href="#">Контакты</a></li>\n' +
-    '        <li class="liHeader"><a id="navButt" href="./login.htm" class="button blueBack" onclick="onLogin()">Войти</a></li>\n' +
+    '        <li class="liHeader"><a id="navButt" href="./login.html" th:href="@{/login.html}" class="button blueBack" onclick="onLogin()">Войти</a></li>\n' +
     '    </ul>\n' +
     '</header>';
 
