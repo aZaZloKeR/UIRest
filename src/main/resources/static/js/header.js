eee72a3d555edb1fe1697fc6821e1fdee89291d9
Alex <62909177+Hesher73@users.noreply.github.com>
2022-04-24 10:21:42 +0300

    add contacts and rules pages

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -13,9 +13,9 @@ var headerText =
     '                <input type="search" placeholder="Поиск" name="search" class="search_box">\n' +
     '            </form>\n' +
     '        </li>\n' +
-    '        <li class="liHeader"><a class="nav" href="#">Предложить инициативу</a></li>\n' +
-    '        <li class="liHeader"><a class="nav" href="#">Правила работы</a></li>\n' +
-    '        <li class="liHeader"><a class="nav" href="#">Контакты</a></li>\n' +
+    '        <li class="liHeader"><a class="nav" href="#" id="proposeLink">Предложить инициативу</a></li>\n' +
+    '        <li class="liHeader"><a class="nav" href="./rulesOfWork.html" id="rulesLink">Правила работы</a></li>\n' +
+    '        <li class="liHeader"><a class="nav" href="./Contacts.html" id="contactsLink">Контакты</a></li>\n' +
     '        <li class="liHeader"><a id="navButt" href="./login.html" th:href="@{/login.html}" class="button blueBack" onclick="onLogin()">Войти</a></li>\n' +
     '    </ul>\n' +
     '</header>';
